1792117f230d2a96afa017fffd637fd58dfbb83d
rafepel <rafepel@gmail.com>
2012-05-22 00:33:40 +0000

    inicio do projeto android

---
+++ streaming-mobile-android/src/streaming/android/StreamingMobileAndroidActivity.java
@@ -0,0 +1,14 @@
+package streaming.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StreamingMobileAndroidActivity extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+	
+    }
+}
